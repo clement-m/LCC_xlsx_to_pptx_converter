a0c3b69f1b1cb2fb191682254b5c3244d0a02bf3
h8R4UFaGGLN95avWw4Pxm77TND <6fhi92eeb62k2wa@jetable.org>
2019-02-27 11:10:44 +0100

    ok2

---
+++ LCC_xlsx_to_pptx_converter/LCC_xlsx_to_pptx_converter/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LCC_xlsx_to_pptx_converter
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+  }
+}
